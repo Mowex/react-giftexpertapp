24c01146933987964781da9b5538d300ecd3f516
vzapatal <victor@strappberry.com>
2020-08-22 21:45:57 -0500

    first commit

---
+++ src/helpers/getGifts.js
@@ -0,0 +1,18 @@
+const getGifts = async ( category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=QYkrYgtAmZ1jidCCJZSGq2dfMZvPfdSH`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifts = data.map(img =>
+        ({
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        })
+    )
+
+    return gifts;
+}
+
+export default getGifts;
