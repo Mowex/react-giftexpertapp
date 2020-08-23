622265d5951201381616fcbd7c84cb93afd2c2d8
vzapatal <victor@strappberry.com>
2020-08-22 21:48:26 -0500

    docs added

---
+++ docs/precache-manifest.92f883d55671285e2102ad8e5ef57b84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d820eb4edea988762190c2deb570944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4fc4b446748da9e1b98",
+    "url": "/static/css/main.d4168f32.chunk.css"
+  },
+  {
+    "revision": "50c5007e2135538c1738",
+    "url": "/static/js/2.a7fcd019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7fcd019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fc4b446748da9e1b98",
+    "url": "/static/js/main.f341a8b9.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
